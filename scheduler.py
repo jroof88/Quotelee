63c0902e498ce2e8a83b2480a489b62a255e8985
John Roof <jroof@scu.edu>
2018-01-10 21:37:36 -0800

    Changed clock time for testing v3

--- scheduler.py
+++ scheduler.py
@@ -1,13 +1,14 @@
-from apscheduler.schedulers.blocking import BackgroundScheduler
+from apscheduler.schedulers import BackgroundScheduler
 from util.daily_quote import send_daily_quote
 
 schedule = BackgroundScheduler(timezone="Pacific Standard Time")
 
-@schedule.scheduled_job('cron', day_of_week='mon-fri', hour=21, minute=35)
+@schedule.scheduled_job('cron', day_of_week='mon-fri', hour=21, minute=40)
 def scheduled_job():
+    print("Sending Out Daily Quote")
     send_daily_quote()
 
-sched.start()
+schedule.start()
 
 #TWILIO_AUTH_TOKEN=93dd11bb635a7cd93db0ad3ad1b1f562
 #TWILIO_ACCOUNT_SID=AC758d54dd40c2ea9cfd57bd7c34a7ebf8
