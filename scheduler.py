19f79d70cbc8312ebf903ba7e4a883e0c7b5dd8d
John Roof <jroof@scu.edu>
2018-01-10 21:19:43 -0800

    Changed clock time for testing

--- scheduler.py
+++ scheduler.py
@@ -3,8 +3,12 @@ from util.daily_quote import send_daily_quote
 
 schedule = BackgroundScheduler(timezone="Pacific Standard Time")
 
-@schedule.scheduled_job('cron', day_of_week='mon-fri', hour=9)
+@schedule.scheduled_job('cron', day_of_week='mon-fri', hour=21, minute=25)
 def scheduled_job():
     send_daily_quote()
 
 sched.start()
+
+#TWILIO_AUTH_TOKEN=93dd11bb635a7cd93db0ad3ad1b1f562
+#TWILIO_ACCOUNT_SID=AC758d54dd40c2ea9cfd57bd7c34a7ebf8
+#TWILIO_PHONE_NUMBER=19147524388
