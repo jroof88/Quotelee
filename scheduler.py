6282ad565818e568f6aca0dab51d296e842229ae
John Roof <jroof@scu.edu>
2018-01-23 19:16:54 -0800

    Heroku scheduler edits

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from util.daily_quote import send_daily_quote
 
-schedule = BlockingScheduler(timezone="Pacific Standard Time")
+sched = BlockingScheduler()
 
-@schedule.scheduled_job('cron', day_of_week='mon-fri', hour=21, minute=50)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=21, minute=50)
 def scheduled_job():
     print("Sending Out Daily Quote")
     send_daily_quote()
 
-schedule.start()
+@sched.scheduled_job('interval', day_of_week='mon-fri', minutes=5)
+def scheduled_job():
+    print("Sending Out Daily Quote")
+    send_daily_quote()
 
-#TWILIO_AUTH_TOKEN=93dd11bb635a7cd93db0ad3ad1b1f562
-#TWILIO_ACCOUNT_SID=AC758d54dd40c2ea9cfd57bd7c34a7ebf8
-#TWILIO_PHONE_NUMBER=19147524388
+sched.start()
