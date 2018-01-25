996c1f6874b4d2220ff51b23bb92bdcfc5862185
John Roof <jroof@scu.edu>
2018-01-24 19:40:52 -0800

    scheduler testing v10 -- Background

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.blocking import BackgroundScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from util.daily_quote import send_daily_quote
 
 sched = BackgroundScheduler()
