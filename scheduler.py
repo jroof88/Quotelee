bb2b205b2bef68130ccc9b52236295748a7b55c0
John Roof <jroof@scu.edu>
2018-01-24 19:24:27 -0800

    scheduler testing v3

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@ from util.daily_quote import send_daily_quote
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=9)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=19, minute=25)
 def scheduled_job():
     print("Sending Out Daily Quote")
     send_daily_quote()
