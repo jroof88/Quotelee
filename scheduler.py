30471cf323043d93aa00664344d1931387aa5f54
John Roof <jroof@scu.edu>
2018-01-24 19:37:56 -0800

    scheduler testing v7 -- Background

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,7 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.blocking import BackgroundScheduler()
 from util.daily_quote import send_daily_quote
 
-sched = BlockingScheduler()
+sched = BackgroundScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=19, minute=35)
 def scheduled_job():
