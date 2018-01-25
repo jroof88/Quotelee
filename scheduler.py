638b2a66e4084866f42ab81fdb6a2b676f2e7595
John Roof <jroof@scu.edu>
2018-01-24 19:38:31 -0800

    scheduler testing v8 -- Background

--- scheduler.py
+++ scheduler.py
@@ -3,8 +3,13 @@ from util.daily_quote import send_daily_quote
 
 sched = BackgroundScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=19, minute=35)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=19, minute=41)
 def scheduled_job():
+    print("Sending Out Daily Quote")
+    send_daily_quote()
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=19, minute=43)
+def scheduled_job2():
     print("Sending Out Daily Quote")
     send_daily_quote()
 
