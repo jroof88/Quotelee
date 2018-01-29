62c776fd3478679320373108a91e44dca4fbe1ac
John Roof <jroof@scu.edu>
2018-01-29 12:24:34 -0800

    Background attempt

--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,9 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from util.daily_quote import send_daily_quote
 
-sched = BlockingScheduler()
+sched = BackgroundScheduler()
 
-@sched.scheduled_job('cron', hour=12, minute=22, timezone='US/Pacific')
+@sched.scheduled_job('cron', hour=12, minute=40, timezone='US/Pacific')
 def send_daily_quote_job():
     print("Sending Out Daily Quote from Cron PST")
     send_daily_quote()
