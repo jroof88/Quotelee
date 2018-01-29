0eb9ecef2c63b890fd2e134d0e5d74829607a597
John Roof <jroof@scu.edu>
2018-01-29 11:58:02 -0800

    blocking attempt5 -- timezone fix

--- scheduler.py
+++ scheduler.py
@@ -1,21 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from util.daily_quote import send_daily_quote
 
-sched = BlockingScheduler()
+sched = BlockingScheduler(timezone="America/California")
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=11, minute=55)
+@sched.scheduled_job('cron', day_of_week='mon', hour=12)
 def send_daily_quote_job():
     print("Sending Out Daily Quote from Cron")
     send_daily_quote()
     
-@sched.scheduled_job('cron', day_of_week='mon', hour=11, minute=57)
+@sched.scheduled_job('cron', day_of_week='mon', hour=12, minute=1)
 def send_daily_quote_job():
     print("Sending Out Daily Quote from Cron")
-    send_daily_quote()
-    
-@sched.scheduled_job('interval', minutes=3)
-def send_daily_quote_job2():
-    print("Sending Out Daily Quote from Interval")
     send_daily_quote()
 
 sched.start()
