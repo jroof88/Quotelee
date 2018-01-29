eb2e6885049f763855ddf9200e35b3d9c5726831
John Roof <jroof@scu.edu>
2018-01-29 12:05:15 -0800

    PST

--- scheduler.py
+++ scheduler.py
@@ -2,15 +2,14 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from util.daily_quote import send_daily_quote
 
 sched = BlockingScheduler(timezone="US/Pacific")
+sched.start()
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=12)
+@sched.scheduled_job('cron', day_of_week='mon', hour=12, minute=7)
 def send_daily_quote_job():
     print("Sending Out Daily Quote from Cron")
     send_daily_quote()
     
-@sched.scheduled_job('cron', day_of_week='mon', hour=12, minute=1)
+@sched.scheduled_job('cron', day_of_week='mon', hour=12, minute=8)
 def send_daily_quote_job():
     print("Sending Out Daily Quote from Cron")
     send_daily_quote()
-
-sched.start()
