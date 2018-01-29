4e927ae9053c38734aa985acc052cc511cc33dd4
John Roof <jroof@scu.edu>
2018-01-29 11:37:07 -0800

    scheduler testing v12 shceduler

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,10 @@
-from apscheduler.schedulers import Scheduler
+from apscheduler.scheduler import Scheduler
 from util.daily_quote import send_daily_quote
 
 sched = Scheduler()
 sched.start()
 
-@sched.cron_schedule('cron', hour=11, minute=37)
+@sched.cron_schedule('cron', hour=11, minute=40)
 def scheduled_job():
     print("Sending Out Daily Quote")
     send_daily_quote()
