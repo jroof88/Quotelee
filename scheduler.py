f1b32430250265ffb4906b0b876d1fdc71fda583
John Roof <jroof@scu.edu>
2018-01-29 12:10:24 -0800

    utc issue

--- scheduler.py
+++ scheduler.py
@@ -2,14 +2,15 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from util.daily_quote import send_daily_quote
 
 sched = BlockingScheduler(timezone="US/Pacific")
-sched.start()
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=12, minute=7)
+@sched.scheduled_job('cron', hour=12, minute=13)
 def send_daily_quote_job():
-    print("Sending Out Daily Quote from Cron")
+    print("Sending Out Daily Quote from Cron 12")
     send_daily_quote()
     
-@sched.scheduled_job('cron', day_of_week='mon', hour=12, minute=8)
+@sched.scheduled_job('cron', hour=4, minute=13)
 def send_daily_quote_job():
-    print("Sending Out Daily Quote from Cron")
+    print("Sending Out Daily Quote from Cron 4")
     send_daily_quote()
+    
+sched.start()
