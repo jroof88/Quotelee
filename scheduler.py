74f5acc59db1ee629cd931558b70af13f1a5b0df
John Roof <jroof@scu.edu>
2018-01-29 12:20:14 -0800

    Timezone issue

--- scheduler.py
+++ scheduler.py
@@ -1,17 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from util.daily_quote import send_daily_quote
-from pytz import utc
 
-sched = BlockingScheduler(timezone=utc)
+sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=12, minute=14)
+@sched.scheduled_job('cron', hour=12, minute=22, timezone='US/Pacific')
 def send_daily_quote_job():
     print("Sending Out Daily Quote from Cron PST")
-    send_daily_quote()
-    
-@sched.scheduled_job('cron', hour=8, minute=14)
-def send_daily_quote_job():
-    print("Sending Out Daily Quote from Cron UTC")
     send_daily_quote()
     
 sched.start()
