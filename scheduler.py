7f11c15d95b142e063c40ff8313c8af6bc4ce15e
John Roof <jroof@scu.edu>
2018-01-29 11:47:34 -0800

    blocking attempt4

--- scheduler.py
+++ scheduler.py
@@ -3,12 +3,12 @@ from util.daily_quote import send_daily_quote
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=11, minute=47)
+@sched.scheduled_job('cron', day_of_week='mon', hour=11, minute=50)
 def send_daily_quote_job():
     print("Sending Out Daily Quote")
     send_daily_quote()
     
-@sched.scheduled_job('interval', minute=3)
+@sched.scheduled_job('interval', minutes=3)
 def send_daily_quote_job2():
     print("Sending Out Daily Quote")
     send_daily_quote()
