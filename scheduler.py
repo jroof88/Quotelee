c642efeab9046167c53514a2111656da1553cf14
John Roof <jroof@scu.edu>
2018-01-29 16:40:51 -0800

    1st production commit

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@ from util.daily_quote import send_daily_quote
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=16, minute=39, timezone='US/Pacific')
+@sched.scheduled_job('cron', hour=9, timezone='US/Pacific')
 def send_daily_quote_job():
     send_daily_quote()
     
