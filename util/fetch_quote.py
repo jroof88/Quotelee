24b5f98a90c8f18761d6c4154adb7d6e2ee37e8f
John Roof <jroof@scu.edu>
2018-02-07 11:10:58 -0800

    Final test

--- util/fetch_quote.py
+++ util/fetch_quote.py
@@ -3,8 +3,6 @@ from nlp import process_input
 from knn import find_nearest_neighbor
 from db import get_collection, get_documents
 
-#https://still-mountain-68428.herokuapp.com/sms_reply
-
 def fetch_quote(input_query):
     tags = process_input(input_query)
     result_id =  find_nearest_neighbor(tags)
@@ -20,6 +18,7 @@ def error_message():
     adjectives = ["life", "sadness", "consciousness", "world"]
     rand1 = randint(0, len(people)-1)
     rand2 = randint(0, len(adjectives)-1)
-    quote['quote'] = "My apologies, I could not find a good match to your search query. Please check spelling or try stuff like \"" + people[rand1] + "\" or \"" + adjectives[rand2] + "\""
-    quote['author'] = 'No Match'
+    error_quote = {}
+    error_quote['quote'] = "My apologies, I could not find a good match to your search query. Please check spelling or try stuff like \"" + people[rand1] + "\" or \"" + adjectives[rand2] + "\""
+    error_quote['author'] = "No Match"
     return quote
